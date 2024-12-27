b937d94eb0b91a8c5227d40ddcd1c5c17b7ae7f6
Build <402271552@qq.com>
2024-12-27 18:01:13 +0800

    process

---
+++ Controllers/sli_work_processController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WebApi_SY.Entity;
+using WebApi_SY.Models;
+
+namespace WebApi_SY.Controllers
+{
+    public class sli_work_processController : ApiController   // 创建工艺流转卡
+    {
+        
+    }
+}
---
+++ Controllers/sli_work_reportController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WebApi_SY.Entity;
+using WebApi_SY.Models;
+
+namespace WebApi_SY.Controllers
+{
+    public class sli_work_reportController : ApiController
+    {
+        
+    }
+}
