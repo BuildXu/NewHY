d272a8b5407bff01f97c8008390575b523b7aa83
Build <402271552@qq.com>
2025-01-14 08:36:51 +0800

    12

--- Models/sli_witnessing_objectbill_view.cs
+++ Models/sli_witnessing_objectbill_view.cs
@@ -17,15 +17,11 @@ namespace WebApi_SY.Models
         public string Fdescription { get; set; } //  规格型号
         public int Fentryid { get; set; } //  销售订单行id
         public int Id { get; set; }      // 数据id  自增
-
         public int Fseq { get; set; }  //  行号  
-
         public int Fobject { get; set; }   //  sli_bd_process_object  /  id
-
         public string Fnote { get; set; }   //  说明  
         public string Fobjectno { get; set; }   //  说明  
         public string Fobjectname { get; set; }   //  说明  
-
         public int Fstatus { get; set; }   //  状态
 
     }
