23e5934a89f9177f4c9caa477e7b5cde7b2ddeab
Build <402271552@qq.com>
2025-01-20 15:04:42 +0800

    111

--- Models/sli_unconformify.cs
+++ Models/sli_unconformify.cs
@@ -26,4 +26,28 @@ namespace WebApi_SY.Models
         public int Fworkorderlistid { get; set; }//工件ID
         public int Fproductno { get; set; }//工件编号
     }
+    public class sli_unconformify_view  //不合格品处理单
+    {
+
+        public int Id { get; set; }    //自增ID
+        public string Fnumber { get; set; }  // 锻件编号
+        public string FnameSpec { get; set; } //名称规格
+        public string FmaterialNo { get; set; }//材质
+        public string FMarcrialspec { get; set; }//用料规格
+        public string FtackingNo { get; set; }//材料跟踪号
+        public int? Ftotalnumber { get; set; }//计划数
+        public string Fprocess { get; set; }//所在工序
+        public string Fposition { get; set; }//工件位置
+        public int? Funnumber { get; set; }//不合格数
+        public string Fdescription { get; set; }//产品不合格描述
+        public int? Fdepid { get; set; }//责任部门ID
+        public string Fdepname { get; set; }//责任部门
+        public int? Fresponsibleid { get; set; }//责任人ID
+        public string Fresponsiblename { get; set; }//责任人
+        public int? Finspector { get; set; }//检验员ID
+        public string Finspectorname { get; set; }//检验员
+        public string FInopinion { get; set; }//检验员意见
+        public int Fworkorderlistid { get; set; }//工件ID
+        public int Fproductno { get; set; }//工件编号
+    }
 }
